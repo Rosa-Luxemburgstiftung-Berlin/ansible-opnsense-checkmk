02c07acf428ece4d7c68bc33e3df8ac7c3f1b370
Mustafa Naier <13101533+mnaier@users.noreply.github.com>
2025-02-14 12:31:38 +0100

    Improved OPENVPN_PORT var to read port number from config file

--- files/check_openvpn.sh
+++ files/check_openvpn.sh
@@ -1,20 +1,25 @@
 #!/usr/local/bin/bash
 # Define variables
 declare -r VERSION=$(openvpn --version 2>/dev/null | head -n 1 | cut -d' ' -f1-3)
-declare -i OPENVPN_PORT=${OPENVPN_PORT:-1194}
+declare -i OPENVPN_PORT=$(sed -nE 's/^port[[:space:]]+([0-9]+)$/\1/p' /var/etc/openvpn/*.conf)
 declare -r OPENVPN_STATUS=$(pluginctl -s openvpn status 2>&1)
 declare -ri ECODE=$?
 
 # Function to check active connections
 check_connections() {
     if command -v netstat > /dev/null; then
-	CONNECTIONS=$(netstat -an 2>/dev/null | grep -E "ESTABLISHED" | grep ":${OPENVPN_PORT}" | wc -l)
+        connections=$(netstat -an 2>/dev/null | grep -E "ESTABLISHED" | grep -w ${OPENVPN_PORT} | wc -l | sed 's/^ *//')
     else
         echo "Error: netstat command is not available on this system."
         exit 1
     fi
-    echo "$CONNECTIONS"
+
+    echo "$connections"
 }
+
+if [ -z "$OPENVPN_PORT" ] || [ "$OPENVPN_PORT" -eq 0 ]; then
+    OPENVPN_PORT=1194
+fi
 
 # Check pluginctl command success
 if [ $ECODE -gt 0 ]; then
@@ -29,9 +34,9 @@ fi
 
 # Check if OpenVPN is running
 if echo "$OPENVPN_STATUS" | grep -q "is running"; then
-    CONNECTIONS=$(check_connections)
+    connections=$(check_connections)
     if [ $? -eq 0 ]; then
-        echo "0 OpenVPN_Status - OpenVPN is running. Active connections: $CONNECTIONS, Version: $VERSION"
+        echo "0 OpenVPN_Status - OpenVPN is running. Active connections: $connections, Version: $VERSION"
     else
         echo "1 OpenVPN_Status - OpenVPN is running, but failed to retrieve active connections."
         exit 1
