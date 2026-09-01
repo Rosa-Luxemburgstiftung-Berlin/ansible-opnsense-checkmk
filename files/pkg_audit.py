eb37be2567e2056e8698c04661bfbd02fe0a0d5c
Klaus Zerwes <zerwes@users.noreply.github.com>
2026-09-01 13:21:52 +0200

    pkg_audit check - output fixed in error case

--- files/pkg_audit.py
+++ files/pkg_audit.py
@@ -68,7 +68,7 @@ pr = subprocess.run(
 try:
     vulnx = json.loads(pr.stdout)
 except Exception as e:
-   print('3 PKGAUDIT UNKNOWN - %s %s' % (e, pr.stderr))
+   print('3 PKGAUDIT - UNKNOWN - %s %s' % (e, pr.stderr))
    sys.exit(3)
 
 vuln_pkg_count = vulnx['pkg_count']
